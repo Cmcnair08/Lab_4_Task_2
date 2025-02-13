8a1e649a6f1055d699dca084e72514a6ddb93f76
ChristianMcNair <mcnairce@mail.uc.edu>
2025-02-13 12:28:56 -0500

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Enter maintenance costs for each season:");
+
+        System.out.print("Spring: $");
+        double springCost = 200;
+
+        System.out.print("Summer: $");
+        double summerCost = 250;
+
+        System.out.print("Autumn: $");
+        double autumnCost = 300;
+
+        System.out.print("Winter: $");
+        double winterCost = 400;
+
+        double totalCost = springCost + summerCost + autumnCost + winterCost;
+
+        System.out.println("Seasonal Maintenance Costs:");
+        System.out.println("Spring: $" + springCost);
+        System.out.println("Summer: $" + summerCost);
+        System.out.println("Autumn: $" + autumnCost);
+        System.out.println("Winter: $" + winterCost);
+        System.out.println("Total Annual Cost: $" + totalCost);
     }
+
 }
+
